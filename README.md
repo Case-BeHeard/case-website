60fce12214f6154c6a7cf9be3a3e5450dc7437f9
muktarkabir <mukhtarkabir3394@gmail.com>
2024-12-12 13:02:33 +0100

    add files

---
+++ README.md
@@ -0,0 +1,2 @@
+# landing-page
+Case landing page
---
+++ script.js
@@ -0,0 +1,15 @@
+const sidebar = document.querySelector(".sidebar");
+
+
+function openGithub() {
+  window.open('https://github.com/ahmadu-suleiman/CASE');
+}
+
+function showSidebar (){
+  sidebar.style.display = 'flex';
+}
+
+function hideSidebar(){
+  sidebar.style.display = 'none';
+}
+
